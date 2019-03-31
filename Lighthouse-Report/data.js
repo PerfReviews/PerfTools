0d679e22fb49d2b0da3439e31c69a22ca0d17295
Joan León <joan.leon@gmail.com>
2019-03-31 14:22:24 +0200

    Added new sites

--- Lighthouse-Report/data.js
+++ Lighthouse-Report/data.js
@@ -1,5 +1,7 @@
 module.exports = {
   mediamarkt: 'https://www.mediamarkt.es/',
   elcorteingles: 'https://www.elcorteingles.es/',
-  zara: 'https://www.zara.com/es/'
+  zara: 'https://www.zara.com/es/',
+  smashingmagazine: 'https://www.smashingmagazine.com/',
+  renfe: 'http://www.renfe.com/'
 }
